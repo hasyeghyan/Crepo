5fa576c243e2d3f68013fa97dd8246d17ec124be
hasyeghyan <hasmikyeghyan05@gmail.com>
2025-07-27 17:06:25 +0400

    Miimum and maximum indexs of array

---
+++ arrays/min_and_max_index.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    const int size = 5;
+    int arr[size]={};
+
+    printf("Input your array elements: ");
+    for(int i = 0; i < size; ++i)
+       scanf("%d", &arr[i]);
+
+    int min  = arr[0];
+    int max = arr[0];
+    int min_index = 0;
+    int max_index = 0;
+
+    for(int i = 0; i < size; ++i){
+       min = arr[i] < min ? arr[i] : min;
+       if(min == arr[i])
+	  min_index = i;
+
+       max = arr[i] > max ? arr[i] : max;
+       if(max == arr[i])
+	  max_index = i;
+ }
+
+       printf("Minimum element's index  is %d\n", min_index);
+       printf("Maximum element's index  is %d\n", max_index);
+
+
+
+
+   return  0;
+}
+
