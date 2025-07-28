8290f5de3813550abfc49f6ec7ee4f4402a79deb
hasyeghyan <hasmikyeghyan05@gmail.com>
2025-07-28 18:21:16 +0400

    array

---
+++ arrays/first_repeated.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    
+   const int size = 7;
+   char arr[size] ={};
+   
+   printf("Please input your array: ");
+   for(int i = 0; i < size; i++){
+       scanf(" %c", &arr[i]);
+   }
+   
+   int current_index = 0;
+   int index = size - 1;
+   
+   for(int i = 0; i < size; i++){
+       for(int j = i + 1; j < size ; j++){
+          if(arr[i] == arr[j]){
+	     current_index  = j;
+	     break;
+	  }
+      }
+   
+	  if (current_index && current_index < index)
+	     index = current_index;
+     
+  }
+   
+   if (!current_index)
+       printf("No repeating characters\n");
+   else
+       printf("First repeating character is %c\n", arr[index]);
+
+   return 0;
+}
