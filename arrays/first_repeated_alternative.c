9e5455b4c06cc3e380b7753891dc40473a751594
hasyeghyan <hasmikyeghyan05@gmail.com>
2025-07-29 22:47:10 +0400

    array

---
+++ arrays/first_repeated_alternative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+   const int size = 7;
+   char arr[size] ={};
+   int arr1[26] = {};
+   int check[26] = {};
+
+   printf("Please input your array: ");
+   for(int i = 0; i < size; i++){
+       scanf(" %c", &arr[i]);
+   }
+
+   for(int i = 0; i < size; i++){
+       arr1[arr[i] - 97] += 1;
+   }
+   
+   for(int i = 0; i < size; i++){
+       if(!check[arr[i] - 97]){
+	  check[arr[i] - 97] = 1;
+          
+       }
+       else{
+	  printf("First repeating character is %c\n", arr[i]);
+	  return 0;
+       }
+           
+   }
+	 printf("No repeting charcters\n");
+
+
+
+   return 0;
+}
+
